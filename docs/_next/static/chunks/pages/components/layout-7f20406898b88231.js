8dca4e5d73af00bbf43178c1c9971f559da71a77
Punn Kamolyabutr <punn.kamolyabutr@gmail.com>
2022-02-26 10:43:40 -0800

    rename

---
+++ docs/_next/static/chunks/pages/components/layout-7f20406898b88231.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[389],{7263:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/layout",function(){return u(5776)}])}},function(n){n.O(0,[774,888,179],(function(){return _=7263,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
