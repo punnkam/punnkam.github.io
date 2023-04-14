f72551d14230354fb5deb849083e6c27d6f017d9
Punn Kamolyabutr <punn.kamolyabutr@gmail.com>
2023-04-13 17:53:30 -0700

    feat: add

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,24 @@ export default function Home() {
             <div className={styles.main}>
                 <p>
                     I&apos;m studying Computer Science at UC San Diego.
-                    Previously I worked on software engineering at Google, the
-                    Scripps lab, and several fintech companies. I&apos;m
-                    especially interested in startups, economics, and crypto.
+                    Previously I worked on backend infra for Search and Ads at
+                    Google, the Scripps lab, and several fintech companies.
+                    I&apos;m especially interested in startups, economics, LLMs,
+                    and crypto.
                 </p>
 
-                <div>
-                    Last summer, I started Blockchain at San Diego, a community
-                    of 500+ builders, researchers, and investors.
-                </div>
+                {/* <div>
+                    In 2021, I started{" "}
+                    <a href="https://twitter.com/BlockchainAtSd">
+                        Blockchain at San Diego
+                    </a>
+                    , a community of 600+ builders, researchers, and investors.
+                </div> */}
 
                 <p>Feel free to reach out!</p>
                 <div className={styles.socials}>
                     <a href="mailto: punn.kamolyabutr@gmail.com">Email</a> —{" "}
-                    <a href="https://twitter.com/xpunnk">Twitter</a> —{" "}
+                    <a href="https://twitter.com/0xpunnk">Twitter</a> —{" "}
                     <a href="https://linkedin.com/in/punnkam">LinkedIn</a>
                 </div>
             </div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,81 +2,96 @@ import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Projects</title>
-        <meta name="description" content="Punn Kam's Projects" />
-        <link rel="icon" href="/logo.jpg" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Projects</title>
+                <meta name="description" content="Punn Kam's Projects" />
+                <link rel="icon" href="/logo.jpg" />
+            </Head>
 
-      <main className={styles.main}>
-        <div className={styles.grid}>
-          <a href="https://triton-spanner.streamlit.app/" className={styles.card}>
-            <h2>UCSD Course Search</h2>
-            <p>
-              Semantic search of UCSD courses using MiniBERT pre-trained encoders.
-            </p>
-            <div className={styles.technologies}>
-              Python
-            </div>
-            <p className={styles.technologies}></p>
-          </a>
-          <a href="https://github.com/prmali/portifi-ethsf" className={styles.card}>
-            <h2>Portifi</h2>
-            <p>
-              Active portfolio manager running an automated trading strategy - ETHSF.
-            </p>
-            <div className={styles.technologies}>
-              Solidity, React, Lambda, EC2
-            </div>
-            <p className={styles.technologies}></p>
-          </a>
-          <a
-            href="https://afternoon-caverns-07461.herokuapp.com/"
-            className={styles.card}
-          >
-            <h2>Home-Service Market </h2>
-            <p>
-              Platform to improve lead generation for home-service businesses.
-            </p>
-            <div className={styles.technologies}>
-              React, Redux, Express, MongoDB
-            </div>
-          </a>
-          <a href="https://bound-eight.vercel.app/" className={styles.card}>
-            <h2>Bound</h2>
-            <p>
-              Gamified NFT subscriptions for crypto native brands and communities.
-            </p>
-            <div className={styles.technologies}>
-              Solidity, Next, Express
-            </div>
-            <p className={styles.technologies}></p>
-          </a>
-          <a href="https://github.com/agora-eco" className={styles.card}>
-            <h2>Agora</h2>
-            <p>
-              Decentralized marketplace for digital and physical goods commerce.
-            </p>
-            <div className={styles.technologies}>
-              Solidity, React, Express, Moralis
-            </div>
-            <p className={styles.technologies}></p>
-          </a>
+            <main className={styles.main}>
+                <div className={styles.grid}>
+                    <a href="https://promptguessr.app/" className={styles.card}>
+                        <h2>PromptGuessr</h2>
+                        <p>
+                            Guess the prompt of a StableDiffusion generated
+                            image scored using embeddings.
+                        </p>
+                        <div className={styles.technologies}>
+                            Solidity, React, Express, Moralis
+                        </div>
+                        <p className={styles.technologies}></p>
+                    </a>
+                    <a
+                        href="https://triton-spanner.streamlit.app/"
+                        className={styles.card}
+                    >
+                        <h2>Triton Course Search</h2>
+                        <p>
+                            Semantic search of UCSD courses using MiniBERT
+                            pre-trained encoders.
+                        </p>
+                        <div className={styles.technologies}>Python</div>
+                        <p className={styles.technologies}></p>
+                    </a>
+                    <a
+                        href="https://github.com/prmali/portifi-ethsf"
+                        className={styles.card}
+                    >
+                        <h2>Portifi</h2>
+                        <p>
+                            Active portfolio manager running an automated
+                            trading strategy - ETHSF.
+                        </p>
+                        <div className={styles.technologies}>
+                            Solidity, React, Lambda, EC2
+                        </div>
+                        <p className={styles.technologies}></p>
+                    </a>
+                    <a
+                        href="https://afternoon-caverns-07461.herokuapp.com/"
+                        className={styles.card}
+                    >
+                        <h2>Home-Service Market </h2>
+                        <p>
+                            Platform to improve lead generation for home-service
+                            businesses.
+                        </p>
+                        <div className={styles.technologies}>
+                            React, Redux, Express, MongoDB
+                        </div>
+                    </a>
+                    <a
+                        href="https://bound-eight.vercel.app/"
+                        className={styles.card}
+                    >
+                        <h2>Bound</h2>
+                        <p>
+                            Gamified NFT subscriptions for crypto native brands
+                            and communities.
+                        </p>
+                        <div className={styles.technologies}>
+                            Solidity, Next, Express
+                        </div>
+                        <p className={styles.technologies}></p>
+                    </a>
 
-          <a href="https://github.com/punnkam/auto-arb" className={styles.card}>
-            <h2>Crypto Arbitrage Bot</h2>
-            <p>
-              Trading bot arbitraging the futures premium on CEX perpetual
-              swaps.
-            </p>
-            <div className={styles.technologies}>
-              Python, Pandas, Numpy, Backtrader
-            </div>
-          </a>
+                    <a
+                        href="https://github.com/punnkam/auto-arb"
+                        className={styles.card}
+                    >
+                        <h2>Crypto Arbitrage Bot</h2>
+                        <p>
+                            Trading bot arbitraging the futures premium on CEX
+                            perpetual swaps.
+                        </p>
+                        <div className={styles.technologies}>
+                            Python, Pandas, Numpy, Backtrader
+                        </div>
+                    </a>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
